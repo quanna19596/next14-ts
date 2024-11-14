09918f3076e303aae72b445c8dc1eae7cf5b669c
quanna <quan.na@shb.com.vn>
2024-11-14 09:57:33 +0700

    .

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,7 +5,7 @@ import { TButtonProps } from './Button.d';
 import './Button.scss';
 
 const Button: React.FC<TButtonProps> = () => {
-  return <button className='Button'>Button</button>;
+  return <button className='Button'>Button client</button>;
 };
 
 export default Button;
